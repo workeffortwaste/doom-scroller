7ba208e66499c1e0e3f971a5cc3f2ef75ca5c124
Chris Johnson <c.johnson@bamboonine.co.uk>
2020-07-22 09:08:38 +0100

    Added a gulp task to package up the web extension.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp')
 const uglifycss = require('gulp-uglifycss')
 const terser = require('gulp-terser')
 const rename = require('gulp-rename')
+const zip = require('gulp-zip')
 
 gulp.task('minify-css', function () {
   return gulp.src(
@@ -20,6 +21,14 @@ gulp.task('minify-js', function () {
     }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./'))
+})
+
+gulp.task('package-web-extension', function () {
+  return gulp.src(['./doom-res/*', './webext/*', 'doom-*.js', 'manifest.json'], {
+    base: './'
+  })
+    .pipe(zip('doom-scroller-web-extension.zip'))
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('default', gulp.series('minify-css', 'minify-js'), function () {})
