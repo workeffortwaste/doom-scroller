9ba4492f5956025f9b878b3de8fbaf81db59864f
Chris Johnson <c.johnson@bamboonine.co.uk>
2020-07-22 11:24:55 +0100

    Updated toolbar icon to display when doom-scroller is enabled.

--- webext/background.js
+++ webext/background.js
@@ -21,7 +21,6 @@ chrome.runtime.onInstalled.addListener(function () {
 })
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-  // If the current tab doesn't exist in storage, then add it and launch the doom-scroller.
   const tabDomain = new URL(tab.url).hostname
 
   getFromStorage('sites').then(
@@ -31,7 +30,26 @@ chrome.browserAction.onClicked.addListener(function (tab) {
       })
     })
 
+  // Update the icon
+  chrome.browserAction.setIcon({ path: 'webext/toolbar-enabled-icon32.png', tabId: tab.tabId })
+
+  // Inject the script
   chrome.tabs.executeScript(tab.id, {
     file: 'doom-scroller-1.0.min.js'
+  })
+})
+
+// If the active tab domain is in the storage, then show the enabled icon.
+chrome.tabs.onActivated.addListener(function (tab) {
+  console.log(tab)
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    const tabDomain = new URL(tabs[0].url).hostname
+    getFromStorage('sites')
+      .then(({ sites }) => {
+        const siteList = sites.split('\n')
+        if (siteList.includes(tabDomain)) {
+          chrome.browserAction.setIcon({ path: 'webext/toolbar-enabled-icon32.png', tabId: tab.tabId })
+        }
+      })
   })
 })
