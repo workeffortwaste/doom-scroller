064e1b3955f232a26a0acf5b494c7ee539887a16
Chris Johnson <c.johnson@bamboonine.co.uk>
2020-07-23 09:58:43 +0100

    Stop the code from being injected multiple times into a page.

--- browser-extension/webext-res/background.js
+++ browser-extension/webext-res/background.js
@@ -21,67 +21,76 @@ chrome.runtime.onInstalled.addListener(function () {
 })
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-  const tabDomain = new URL(tab.url).hostname
+    if (tab.url != undefined){
+        const tabDomain = new URL(tab.url).hostname
 
-  const inject = false
+        const inject = false
 
-  getFromStorage('sites').then(
-    e => {
-      const siteList = e.sites.split('\n')
-      // If the does not exist in the sitelist
-      if (!siteList.includes(tabDomain)) {
-        setToStorage({
-          sites: e.sites + '\n' + tabDomain
-        }).then(e => {
-          chrome.browserAction.setIcon({ path: 'webext-res/toolbar-enabled-icon32.png', tabId: tab.tabId })
-          chrome.tabs.executeScript(tab.id, {
-            file: 'doom-scroller-1.1.1.min.js'
-          })
-        })
-      } else {
-        // If it already exists here, then remove it.
-        const filteredArray = siteList.filter(function (e) { return e !== tabDomain })
-        setToStorage({
-          sites: filteredArray.join('\n')
-        })
-        // Update the icon
-        chrome.browserAction.setIcon({ path: 'webext-res/toolbar-icon32.png', tabId: tab.tabId })
-        // Refresh tab
-        const refresh = 'window.location.reload();'
-        chrome.tabs.executeScript(tab.id, { code: refresh })
-      }
-    })
+        getFromStorage('sites').then(
+            e => {
+                const siteList = e.sites.split('\n')
+                // If the does not exist in the sitelist
+                if (!siteList.includes(tabDomain)) {
+                    setToStorage({
+                        sites: e.sites + '\n' + tabDomain
+                    }).then(e => {
+                        chrome.browserAction.setIcon({path: 'webext-res/toolbar-enabled-icon32.png', tabId: tab.tabId})
+                        chrome.tabs.executeScript(tab.id, {
+                            file: 'doom-scroller-1.1.1.min.js'
+                        })
+                    })
+                } else {
+                    // If it already exists here, then remove it.
+                    const filteredArray = siteList.filter(function (e) {
+                        return e !== tabDomain
+                    })
+                    setToStorage({
+                        sites: filteredArray.join('\n')
+                    })
+                    // Update the icon
+                    chrome.browserAction.setIcon({path: 'webext-res/toolbar-icon32.png', tabId: tab.tabId})
+                    // Refresh tab
+                    const refresh = 'window.location.reload();'
+                    chrome.tabs.executeScript(tab.id, {code: refresh})
+                }
+            })
+    }
 })
 // If the active tab domain is in the storage, then show the enabled icon.
 chrome.tabs.onActivated.addListener(function (tab) {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    const tabDomain = new URL(tabs[0].url).hostname
-    getFromStorage('sites')
-      .then(({ sites }) => {
-        const siteList = sites.split('\n')
-        if (siteList.includes(tabDomain)) {
-          chrome.browserAction.setIcon({ path: 'webext-res/toolbar-enabled-icon32.png', tabId: tab.tabId })
-        } else {
-          chrome.browserAction.setIcon({ path: 'webext-res/toolbar-icon32.png', tabId: tab.tabId })
-        }
-      })
-  })
+    if(tab.url != undefined) {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            const tabDomain = new URL(tabs[0].url).hostname
+            getFromStorage('sites')
+                .then(({sites}) => {
+                    const siteList = sites.split('\n')
+                    if (siteList.includes(tabDomain)) {
+                        chrome.browserAction.setIcon({path: 'webext-res/toolbar-enabled-icon32.png', tabId: tab.tabId})
+                    } else {
+                        chrome.browserAction.setIcon({path: 'webext-res/toolbar-icon32.png', tabId: tab.tabId})
+                    }
+                })
+        })
+    }
 })
 
 chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    const tabDomain = new URL(tabs[0].url).hostname
-    getFromStorage('sites')
-      .then(({ sites }) => {
-        const siteList = sites.split('\n')
-        if (siteList.includes(tabDomain)) {
-          chrome.browserAction.setIcon({ path: 'webext-res/toolbar-enabled-icon32.png', tabId: tab.tabId })
-            chrome.tabs.executeScript(tab.id, {
-                file: 'doom-scroller-1.1.1.min.js'
-            })
-        } else {
-          chrome.browserAction.setIcon({ path: 'webext-res/toolbar-icon32.png', tabId: tab.tabId })
-        }
-      })
-  })
+    if(change.status == 'complete' && tab.status == 'complete' && tab.url != undefined) {
+
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            const tabDomain = new URL(tabs[0].url).hostname
+            getFromStorage('sites')
+                .then(({sites}) => {
+                    const siteList = sites.split('\n')
+                    if (siteList.includes(tabDomain)) {
+                        chrome.browserAction.setIcon({path: 'webext-res/toolbar-enabled-icon32.png', tabId: tab.tabId})
+                        chrome.tabs.executeScript(tab.id, {
+                            file: 'doom-scroller-1.1.1.min.js'
+                        })
+                    } else {
+                        chrome.browserAction.setIcon({path: 'webext-res/toolbar-icon32.png', tabId: tab.tabId})
+                    }
+                })
+        })
+    }
 })
